9864e11e7e81d8dfab2135acc2edbad1cc816a9c
kaanaras06 <arasgrupdc@gmail.com>
2026-08-22 11:22:10 +0300

    Delete Official_Links_2026.py

--- Official_Links_2026.py
+++
@@ -1,8 +0,0 @@
-OFFICIAL_LINKS_2026 = {
-    "DE": {"country": "Almanya", "url": "https://idata.com.tr/de/tr", "provider": "iDATA", "note": "Tek yetkili - 18 Mart 2024"},
-    "IT": {"country": "Italya", "url": "https://idata.com.tr/ita/tr", "provider": "iDATA"},
-    "FR": {"country": "Fransa", "url": "https://visa.vfsglobal.com/tur/tr/fra", "provider": "VFS Global"},
-    "NL": {"country": "Hollanda", "url": "https://visa.vfsglobal.com/tur/tr/nld", "provider": "VFS Global"},
-}
-def get_official_link(code):
-    return OFFICIAL_LINKS_2026.get(code, {}).get("url", "")
