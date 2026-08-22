0e31f771a7187ff0bc4281312fedff3e0940c49d
kaanaras06 <arasgrupdc@gmail.com>
2026-08-22 10:53:19 +0300

    Delete alarms-updated.py

--- alarms-updated.py
+++
@@ -1,6 +0,0 @@
-# Alarms - iDATA focused 2026
-ALARMS = [
-    {"country": "DE", "city": "Istanbul", "url": "https://idata.com.tr/de/tr"},
-    {"country": "DE", "city": "Ankara", "url": "https://idata.com.tr/de/tr"},
-    {"country": "IT", "city": "Istanbul", "url": "https://idata.com.tr/ita/tr"},
-]
